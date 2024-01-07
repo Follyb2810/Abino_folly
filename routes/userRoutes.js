4131f8406af92b563c9c177cf6a131e63a166978
follyb2810 <babatundeyusuffolorunsho@gmail.com>
2024-01-07 13:56:34 -0800

    adding changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { SignUp, SignIn, UpdateUser, protectedRoutes, Logout, folly } = require("../controller/authController");
+const { SignUp, SignIn, UpdateUser, protectedRoutes, Logout, folly, SingleUser } = require("../controller/authController");
 const router = express.Router();
 
 router.route("/signUp").post(SignUp)
@@ -7,6 +7,6 @@ router.route("/folly").get(folly)
 router.route("/signIn").post(SignIn)
 router.route("/logOut").get(Logout)
 router.route("/singleUser").get(SingleUser)
-router.route("/update/:userId").put(protectedRoutes,UpdateUser)
+router.route("/update/:userId").put(UpdateUser)
 
 module.exports = router;
